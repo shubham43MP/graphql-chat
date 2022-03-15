89c785e1249e054d504bb8d90f0c72905cb6669c
shubham43MP <shubhamdixit19950321@gmail.com>
2022-03-15 11:22:08 +0530

    Setup Apollo Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
-import { getLoggedInUser, logout } from './auth';
-import Chat from './Chat';
-import Login from './Login';
-import NavBar from './NavBar';
+import React, { Component } from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { getLoggedInUser, logout } from "./auth";
+import Chat from "./Chat";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import client from "./graphql/client";
 
 class App extends Component {
-  state = {user: getLoggedInUser()};
+  state = { user: getLoggedInUser() };
 
   handleLogin(user) {
-    this.setState({user});
+    this.setState({ user });
   }
 
   handleLogout() {
     logout();
-    this.setState({user: null});
+    this.setState({ user: null });
   }
 
   render() {
-    const {user} = this.state;
+    const { user } = this.state;
     if (!user) {
       return <Login onLogin={this.handleLogin.bind(this)} />;
     }
     return (
-      <div>
-        <NavBar onLogout={this.handleLogout.bind(this)} />
-        <Chat user={user} />
-      </div>
-    );  
+      <ApolloProvider client={client}>
+        <div>
+          <NavBar onLogout={this.handleLogout.bind(this)} />
+          <Chat user={user} />
+        </div>
+      </ApolloProvider>
+    );
   }
 }
 
